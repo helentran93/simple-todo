b7168df597a1b4a1c39cb41203f7e8344b0f4054
Helen T <helen.tran@vincit.fi>
2020-06-05 17:02:56 +0300

    Fix: modified actions' functionalities and endpoints to return objects correctly

--- backend/routes/actions.js
+++ backend/routes/actions.js
@@ -1,3 +1,4 @@
+const {v4: uuidv4} = require('uuid');
 let items = [];
 
 module.exports = {
@@ -8,10 +9,16 @@ module.exports = {
         return items.find(item => item.id === itemId);
     },
     createNewItem: (newItem) => {
+        console.log(typeof newItem);
+
+        newItem.id = uuidv4();
+        newItem.order = items.length + 1;
+        console.log(JSON.stringify(newItem));
         return items.push(newItem);
     },
     deleteItem: (itemId) => {
-        return items.filter(item => item.id !== itemId);
+        items = items.filter(item => item.id !== itemId);
+        return items;
     },
     updateItem: (itemId, updated) => {
         const itemIndex = items.findIndex(item => item.id === itemId);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,31 +10,31 @@ router.get('/', function(req, res, next) {
 /* GET all the items. */
 router.get('/items', (req, res) => {
   const response = actions.getItems();
-  res.status(200).send(response);
+  res.status(200).json(response);
 })
 
 /* GET item by id. */
 router.get('/items/:id', (req, res) => {
   const response = actions.getItemById(req.params.id);
-  res.status(200).send(response);
+  res.status(200).json(response);
 })
 
 /* POST new item to list. */
 router.post('/items', (req, res) => {
   const response = actions.createNewItem(req.body);
-  res.status(201).send(response);
+  res.status(201).json(response);
 })
 
 /* DELETE item from list by id. */
 router.delete('/items/:id', (req, res) => {
   const response = actions.deleteItem(req.params.id);
-  res.status(200).send(response);
+  res.status(200).json(response);
 })
 
 /* PUT update an item in the list. */
 router.put('/items/:id', (req, res) => {
   const response = actions.updateItem(req.params.id, req.body);
-  res.status(200).send(response);
+  res.status(200).json(response);
 })
 
 module.exports = router;
